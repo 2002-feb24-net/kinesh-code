072409095f0b8d2b33261370158975678e283c3f
kinesh mistry <kinesh.mistry@gmail.com>
2020-03-03 16:36:50 -0600

    project of rockpaperscissor

---
+++ DemoObject/Product.cs
@@ -0,0 +1,40 @@
+using System;
+namespace DemoObject
+{
+    class Product
+    {
+        string ProductId;
+        int Stock;
+        double StarRating;
+        public void SetDefaultValues()
+        {
+            ProductId = "1";
+            Stock = 0;
+            StarRating = 5;
+        }
+
+        public void SetValues(string id, int quantity, double rating)
+        {
+            // business logic like "no product can have more than 50 quantity"
+            // belongs in the classes that represent those entities.
+            // not in input/output code.
+            if (quantity > 50)
+            {
+                Console.WriteLine("Error! too much quantity");
+            }
+            if (quantity < 0)
+            {
+                Console.WriteLine("Error - can't have negative quantity");
+            }
+
+            ProductId = id;
+            Stock = quantity;
+            StarRating = rating;
+        }
+        public override string ToString()
+        {
+            return "product " + ProductId + ", "
+                + Stock + " in stock, " + StarRating + " star rating";
+        }
+    }
+}
---
+++ Lists/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            int howmany = numbers.Count;
+            numbers.Add(123);
+            int firstItems = numbers[0];
+            numbers[0] = 5;
+            numbers.Insert(0,1000);
+            numbers.Contains(500);//false
+            numbers.Add(500);
+            numbers.Contains(500); //true
+            numbers.RemoveAt(2);//remove third item
+            numbers.Remove(500);//remove first 500 that it finds,searching from the left
+            
+            int sum = 0;
+            foreach( int num in numbers )
+                {
+                    sum += num;
+
+                }
+        }
+    }
+}
